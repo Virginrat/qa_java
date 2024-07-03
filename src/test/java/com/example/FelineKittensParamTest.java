0b160489f19eda9db951a10a8d1d735ff801564f
Virginrat <117899973+Virginrat@users.noreply.github.com>
2024-07-03 14:45:21 +0300

    Add files via upload

---
+++ src/test/java/com/example/FelineKittensParamTest.java
@@ -0,0 +1,35 @@
+package com.example;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
+public class FelineKittensParamTest {
+
+    private final int expected;
+
+    public FelineKittensParamTest(int expected) {
+        this.expected = expected;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> getKittensData() {
+        return Arrays.asList(new Object[][]{
+                {1},
+                {0},
+                {-1},
+        });
+    }
+
+    @Test
+    public void testGetKittensWithArgument() {
+        Feline feline = new Feline();
+        int actual = feline.getKittens(expected);
+        Assert.assertEquals(expected, actual);
+    }
+}
